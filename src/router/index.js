f2fa6f30c011819bf77e6750c845ef613662c51d
Zkeq <admin@icodeq.com>
2023-09-25 17:42:15 +0800

    Add Login pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import About from '@/components/About'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/login',
+      nmae: 'Login',
+      component: Login
     }
   ]
 })
