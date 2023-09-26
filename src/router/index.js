bc4195fe4a85b30d8c95212232a87be303224909
Zkeq <admin@icodeq.com>
2023-09-26 15:46:20 +0800

    Add new pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import About from '@/components/About'
 import Login from '@/components/Login'
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: "history",
   routes: [
     {
       path: '/',
@@ -21,8 +23,19 @@ export default new Router({
     },
     {
       path: '/login',
-      nmae: 'Login',
       component: Login
+    },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
     }
   ]
 })
